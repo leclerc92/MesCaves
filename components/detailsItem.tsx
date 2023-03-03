3ad5fd67f745e7d30adeb73727006f4f068ee9ad
leclerc92 <leclerc92@gmail.com>
2023-03-03 18:12:21 +0100

    modif detailsitem

--- components/detailsItem.tsx
+++ components/detailsItem.tsx
@@ -30,7 +30,7 @@ const DetailsItem: FC<Props> = ({item,genre}): JSX.Element => {
           </label>
           <h1
             className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
-          > {item.nom}</h1>
+          > {item.data.nom}</h1>
         </div>
       </div>
       <div className="w-full px-3 sm:w-1/2">
@@ -43,7 +43,7 @@ const DetailsItem: FC<Props> = ({item,genre}): JSX.Element => {
           </label>
           <h1
             className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
-          > {item.pays}</h1>
+          > {item.data.pays}</h1>
         </div>
       </div>
     </div>
@@ -55,7 +55,7 @@ const DetailsItem: FC<Props> = ({item,genre}): JSX.Element => {
       </label>
       <h1
         className="w-full appearance-none rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
-      >{item.type}</h1>
+      >{item.data.type}</h1>
     </div>
 
     <div className="-mx-3 flex flex-wrap">
@@ -70,7 +70,7 @@ const DetailsItem: FC<Props> = ({item,genre}): JSX.Element => {
           </label>
           <h1
             className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
-          > {genre == "Whiskys" ? item.tourbe : item.sucre}</h1>
+          > {genre == "Whiskys" ? item.data.tourbe : item.data.sucre}</h1>
         </div>
       </div>
       <div className="w-full px-3 sm:w-1/2">
@@ -83,20 +83,20 @@ const DetailsItem: FC<Props> = ({item,genre}): JSX.Element => {
           </label>
           <h1
             className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
-          > {item.note}</h1>
+          > {item.data.note}</h1>
         </div>
       </div>
     </div>
 
      <div className=" flex item-center mb-5 justify-between">
         <label className="relative inline-flex items-center cursor-pointer">
-            <input type="checkbox" readOnly checked={item.stock}  className="sr-only peer"/>
+            <input type="checkbox" readOnly checked={item.data.stock}  className="sr-only peer"/>
              <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
             <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">Stock</span>
         </label>
 
         <label className="relative inline-flex items-center mr-5 cursor-pointer">
-            <input type="checkbox" readOnly checked={item.like} className="sr-only peer"/>
+            <input type="checkbox" readOnly checked={item.data.like} className="sr-only peer"/>
             <div className=" w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-red-300 dark:peer-focus:ring-red-800 dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-red-600"></div>
             <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">Favori</span>
         </label>
@@ -111,12 +111,12 @@ const DetailsItem: FC<Props> = ({item,genre}): JSX.Element => {
           </label>
           <h1
             className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
-          > {item.commentaire}</h1>
+          > {item.data.commentaire}</h1>
     </div>
 
         <div className='flex flex-col items-center'>
           <Link target='blank' href={`https://www.google.fr/search?tbm=shop&hl=fr&psb=1&ved=2ahUKEwjulISXtr39AhVPKNMKHYyWAPMQu-kFegQIABAP&q=${item.nom}&oq=talisker+10ans&gs_lcp=Cgtwcm9kdWN0cy1jYxADUABYAGAAaABwAHgAgAEAiAEAkgEAmAEA&sclient=products-cc`}>
-            <img src={item.image}/>
+            <img src={item.data.image}/>
           </Link>
         </div>
       
